0eed1455828618230e22734c31ba70730196a921
AgustinSalazar <agustinsssalazar@gmail.com>
2023-02-28 13:20:09 -0300

    Terminado punto 1 de la actividad-Clase-1

---
+++ Actividades-Clase-1/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        //  Introducción a algoritmos y java
+        //  Actividad 1:  *** Descomentar para probar cada punto ***
+
+        //a_
+        int numInicio = 5;
+        int numFin = 14;
+        //while (numInicio <= numFin) {
+        //     System.out.println(numInicio);
+        //     numInicio += 1;
+        // }
+
+        //b
+        //while (numInicio <= numFin) {
+        //     if (numInicio % 2 == 0){
+        //         System.out.println(numInicio);
+        //     }
+        //     numInicio += 1;
+        // }
+
+        //c
+        //boolean mostrarPares = true;  //Si mostrarPares esta en true muestra los números pares, si esta en false muestra los impares.
+        // while (numInicio <= numFin) {
+        //     if (mostrarPares){
+        //         if (numInicio % 2 == 0){
+        //             System.out.println(numInicio);
+        //         }
+        //     } else {
+        //         if (numInicio % 2 != 0){
+        //             System.out.println(numInicio);
+        //         }
+        //     }
+        //     numInicio += 1;
+        // }
+
+        //d
+        //for (int otroNumFin=14;otroNumFin >= numInicio;otroNumFin--){
+        //     System.out.println(otroNumFin);
+        // }
+
+        //******************************************************************************************************
+
+        //Actividad 2:
+
+
+    }
+}
